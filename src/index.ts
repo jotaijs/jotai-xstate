4dd90b20dda522fb89e2091a67e8cca9a70f93ca
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-07-12 15:08:07 -0300

    Added missing imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { atomWithMachine } from './atomWithMachine.js';
 export { isGetter, RESTART } from './utils.js';
+export type { Gettable } from './utils.js';
 export { atomWithActor } from './atomWithActor.js';
+export { atomWithActorSnapshot } from './atomWithActorSnapshot.js';
